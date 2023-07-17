adf7c6a25eb739b3838a89ba7991b9bf13cdee53
Mustafa Serhat Demirtaş <93614252+Actionlistener06@users.noreply.github.com>
2023-07-18 02:12:39 +0300

    patpat

--- patika-java-practices/src/KdvHesap.java
+++ patika-java-practices/src/KdvHesap.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class KdvHesap {
+    public static void main(String[] args) {
+        double tutar,kdvOran=0.18;
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Tutar giriniz");
+        tutar=scanner.nextDouble();
+        if(tutar>1000){
+            kdvOran=0.08;
+        }
+        double KDV=tutar*kdvOran;
+        double KDVlitutar=tutar+KDV;
+        System.out.println("KDV tutari: "+KDV+"\nKDV eklenmis tutar: "+KDVlitutar);
+    }
 }
