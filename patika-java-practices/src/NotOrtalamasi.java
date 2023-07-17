2468ad79f2c7e0ea3f9aab8d7f8209285b8c5f40
Mustafa Serhat Demirtaş <93614252+Actionlistener06@users.noreply.github.com>
2023-07-18 01:59:23 +0300

    patpat

--- patika-java-practices/src/NotOrtalamasi.java
+++ patika-java-practices/src/NotOrtalamasi.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class NotOrtalamasi {
+
+import java.util.Scanner;
+
+public class NotOrtalamasi {
+    public static void main(String[] args) {
+        int top=0;
+        int mat,fiz,kim,tur,tar,mus;
+        Scanner scanner  = new Scanner(System.in);
+        System.out.println("Matematik notunu giriniz");
+        mat=scanner.nextInt();
+        System.out.println("Fizik notunu giriniz");
+        fiz=scanner.nextInt();
+        System.out.println("Kimya notunu giriniz");
+        kim=scanner.nextInt();
+        System.out.println("Turkce notunu giriniz");
+        tur=scanner.nextInt();
+        System.out.println("Tarih notunnu giriniz");
+        tar=scanner.nextInt();
+        System.out.println("Muzik noutnu giriniz");
+        mus=scanner.nextInt();
+        top=mat+fiz+kim+tur+tar+mus;
+        double ort=top/6.0;
+        if(ort>=60.0){
+            System.out.println("GECTI");
+        }
+        else System.out.println("KALDI");
+    }
 }
