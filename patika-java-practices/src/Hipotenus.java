a4361e697a9e614c153d272cf1ae8398eec83a54
Mustafa Serhat Demirtaş <93614252+Actionlistener06@users.noreply.github.com>
2023-07-19 21:05:16 +0300

    asd

--- patika-java-practices/src/Hipotenus.java
+++ patika-java-practices/src/Hipotenus.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Hipotenus {
+import java.util.Scanner;
+
+public class Hipotenus {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kenar uzunluklarini giriniz");
+        int a=scanner.nextInt();
+        int b= scanner.nextInt();
+        int c=scanner.nextInt();
+        int cevre=a+b+c;
+        double alan=Math.sqrt(cevre*(cevre-a)*(cevre-b)*(cevre-c));
+        System.out.println(alan);
+    }
 }
